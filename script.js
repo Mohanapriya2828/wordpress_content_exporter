dbede7048751b3e3f66c8e1e1f157b2c04f24d72
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-09-27 15:40:32 +0530

    Bold,italic,underline,heading feature commit

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ class AssignmentEditor {
     this.init();
   }
 
-  init() {
-    console.log("Editor initialized");
-    
+init() {
+     document.querySelectorAll("#toolbar button").forEach(button => {
+      button.addEventListener("click", () => {
+        let command = button.getAttribute("data-command");
+        document.execCommand(command, false, null);
+      });
+    });
+    document.getElementById("heading-select").addEventListener("change", (e) => {
+      let value = e.target.value;
+      if (value) {
+        document.execCommand("formatBlock", false, value);
+      } else {
+        document.execCommand("formatBlock", false, "p");
+      }
+    });
   }
 }
-
 
 document.addEventListener("DOMContentLoaded", () => {
   new AssignmentEditor("editor");
